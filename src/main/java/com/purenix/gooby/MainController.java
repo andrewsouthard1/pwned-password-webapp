86006af32b53654c876be16d5c670eaca7f7f9dd
andrewsouthard1 <andrew.southard@datadoghq.com>
2018-06-14 17:15:41 -0400

    Updated import statement to include Trace method

--- src/main/java/com/purenix/gooby/MainController.java
+++ src/main/java/com/purenix/gooby/MainController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import datadog.trace.api.DDTags;
+import datadog.trace.api.Trace;
 
 @Controller    // This means that this class is a Controller
 @RequestMapping(path="/pwned") // This means URL's start with /pwned (after Application path)
